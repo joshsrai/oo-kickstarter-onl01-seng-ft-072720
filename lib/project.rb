2719e5e574c2f162efc23f65773bf21c784a2116
Josh Raiborde <joshsrai@yahoo.com>
2020-08-13 13:37:33 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,7 +7,25 @@ def initialize(name)
 end
 
 def add_backer(backer)
-  @backers << backer 
+  @backers << backer
+  project.add_backer(self) if project.backers.include?(self) == false
+  
 end
 
+end
+
+
+
+class Backer 
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name  
+    @backed_projects = []
+    end
+    
+    def back_project(project)
+      @backed_projects << project
+      project.add_backer(self) if project.backers.include?(self) == false
+    end
 end
